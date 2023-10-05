b423d2c23dd2cf34a924f1f8f65cae1af22cbeb8
Wolfxela <alexthegreitsas@gmail.com>
2023-10-05 19:20:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const makeDropDown = (function () {
+  const addMenu = function (element, button, menuClass, menuItemClass) {
+    button.addEventListener("click", function () {
+      showElements(element, menuClass, menuItemClass);
+    });
+  };
+  const showElements = function (menu, menuActiveClass, menuActiveItemClass) {
+    const menuList = menu.getElementsByTagName("ul")[0];
+    const menuElements = menuList.getElementsByTagName("li");
+
+    if (menuList.classList.contains(menuActiveClass) === false) {
+      menuList.classList.add(menuActiveClass);
+      for (let i = 0; i < menuElements.length; i++) {
+        menuElements[i].classList.add(menuActiveItemClass);
+      }
+    } else if (menuList.classList.contains(menuActiveClass) === true) {
+      menuList.classList.remove(menuActiveClass);
+      for (let i = 0; i < menuElements.length; i++) {
+        menuElements[i].classList.remove(menuActiveItemClass);
+      }
+    }
+  };
+  return { addMenu };
+})();
+
+export { makeDropDown };
